a93809844362cb0fcb547a3cbf486b2ae563f184
darwish791 <darwish791@gmail.com>
2019-02-26 21:54:54 +0800

    Progress

---
+++ app/src/main/java/com/darwish/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,23 @@
+package com.darwish.flashchatnewfirebase;
+
+public class InstantMessage {
+
+    private String message;
+    private String author;
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
--- app/src/main/java/com/darwish/flashchatnewfirebase/MainChatActivity.java
+++ app/src/main/java/com/darwish/flashchatnewfirebase/MainChatActivity.java
@@ -1,10 +1,18 @@
 package com.darwish.flashchatnewfirebase;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainChatActivity extends AppCompatActivity {
@@ -14,6 +22,7 @@ public class MainChatActivity extends AppCompatActivity {
     private ListView mChatListView;
     private EditText mInputText;
     private ImageButton mSendButton;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,10 @@ public class MainChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_chat);
 
         // TODO: Set up the display name and get the Firebase reference
+
+        setupDiplayName();
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
 
         // Link the Views in the layout to the Java code
@@ -30,17 +43,51 @@ public class MainChatActivity extends AppCompatActivity {
 
         // TODO: Send the message when the "enter" button is pressed
 
+        mInputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                sendMessage();
+                return true;
+            }
+        });
 
         // TODO: Add an OnClickListener to the sendButton to send a message
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
 
     }
 
     // TODO: Retrieve the display name from the Shared Preferences
 
+    private void setupDiplayName() {
+
+        SharedPreferences prefs = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
+
+        mDisplayName = prefs.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
+
+        if (mDisplayName == null) {
+            mDisplayName = "Anonymous";
+        }
+    }
+
 
     private void sendMessage() {
 
         // TODO: Grab the text the user typed in and push the message to Firebase
+
+        Log.d("FlashChat", "I sent something:  ");
+        String input = mInputText.getText().toString();
+        if (!input.equals("")) {
+            InstantMessage chat = new InstantMessage(input, mDisplayName);
+            mDatabaseReference.child("message").push().setValue(chat);
+            mInputText.setText("");
+
+        }
 
     }
 
